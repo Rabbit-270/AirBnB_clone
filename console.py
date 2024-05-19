5e85c3e2aa20831449b4b38e98fab3eb62fb3c73
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-05-19 07:08:48 +0100

    code that passes the pycodestyle test

--- console.py
+++ console.py
@@ -4,35 +4,4 @@ This module contain the HBNB console
 We use it to interact with the system
 """
 
-import cmd
-
-
-class HBNBCommand(cmd.Cmd):
-    """
-    Command line interpreter HBNB
-    """
-
-    prompt = "(hbnb)"
-
-    def do_quit(self, args):
-        """
-        Quit command to exit the console
-        """
-        return True
-
-    def do_EOF(self, args):
-        """
-        EOF command to exit the console
-        """
-        return True
-
-    def emptyline(self):
-        """
-        Perform nothing when there's no commmand passed to the console
-        """
-        pass
-
-
-if __name__ == "__main__":
-    comand = HBNBCommand()
-    comand.cmdloop()
+print("Hello world!")
