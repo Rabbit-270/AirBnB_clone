ad1fd0e9e6d6f42a2cdd3d1f6e5572724c1080d3
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-05-19 06:56:27 +0100

    code that passes the pycodestyle test

--- console.py
+++ console.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""This module contain the HBNB console"""
+"""
+This module contain the HBNB console
+We use it to interact with the system
+"""
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command line interpreter HBNB"""
+    """
+    Command line interpreter HBNB
+    """
 
     prompt = "(hbnb)"
 
     def do_quit(self, args):
-        """Quit command to exit the console\n"""
+        """
+        Quit command to exit the console
+        """
         return True
 
     def do_EOF(self, args):
-        """EOF command to exit the console\n"""
+        """
+        EOF command to exit the console
+        """
         return True
 
     def emptyline(self):
-        """Perform nothing when there's no commmand passed to the console"""
+        """
+        Perform nothing when there's no commmand passed to the console
+        """
         pass
 
 
