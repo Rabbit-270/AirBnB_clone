dc8ec01de3250b4420c857edf73e75ed3cae521a
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-05-19 08:59:27 +0100

    defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,55 @@
 #!/usr/bin/python3
+"""
+Base model
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """Initialize the class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        instanceDict = self.__dict__.copy()
+        instanceDict["__class__"] = self.__class__.__name__
+        instanceDict["created_at"] = self.created_at.isoformat()
+        instanceDict["updated_at"] = self.updated_at.isoformat()
+
+        return instanceDict
+
+    def __str__(self):
+        """
+        Print in string format
+        """
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
+
+
+if __name__ == "__main__":
+    myModel = BaseModel()
+    myModel.name = "My_First_Model"
+    myModel.my_number = 89
+    print(myModel)
+    myModel.save()
+    print(myModel)
+    modelJson = myModel.to_dict()
+    print(modelJson)
+    print("JSON of myModel:")
+    for i in modelJson.keys():
+        print("\t{}: ({}) - {}".format(i, type(modelJson[i]), modelJson[i]))
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""
+Test base model
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        myModel = BaseModel()
+
+        self.assertIsNotNone(myModel.id)
+        self.assertIsNotNone(myModel.created_at)
+        self.assertIsNotNone(myModel.updated_at)
+
+    def test_save(self):
+        myModel = BaseModel()
+
+        initialUpdatedAt = myModel.updated_at
+        currentUpdatedAt = myModel.save()
+
+        self.assertNotEqual(initialUpdatedAt, currentUpdatedAt)
+
+    def test_to_dict(self):
+        model = BaseModel()
+
+        modelDic = model.to_dict()
+
+        self.assertIsInstance(modelDic, dict)
+        self.assertEqual(modelDic["__class__"], "BaseModel")
+        self.assertEqual(modelDic["id"], model.id)
+        self.assertEqual(modelDic["created_at"], model.created_at.isoformat())
+        self.assertEqual(modelDic["updated_at"], model.updated_at.isoformat())
+
+    def test_str(self):
+        myModel = BaseModel()
+
+        self.assertTrue(str(myModel).startswith("[BaseModel]"))
+        self.assertIn(myModel.id, str(myModel))
+        self.assertIn(str(myModel.__dict__), str(myModel))
+
+
+if __name__ == "__main":
+    unittest.main()
