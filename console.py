2ab504e2c009d3da8f01525f8c448fa1a93897ac
APINDA <apinda127@gmail.com>
2024-05-20 16:13:02 -0700

    console, #1

--- console.py
+++ console.py
@@ -5,4 +5,32 @@ This module contain the HBNB console
 We use it to interact with the system
 """
 
-print("Hello world!")
+
+class HBNBCommand(cmd.Cmd):
+    '''
+    Our command line
+    '''
+    def do_EOF(self, Line):
+        '''
+        Parse end-of-file-marker in the hbnb commnd line
+        and return True to terminate the program
+        '''
+        return True
+
+    def do_quit(self, line):
+        '''
+exit the program
+        '''
+        exit()
+
+    prompt = "(hbnb)"
+
+    def emptyline(self):
+        '''
+Executed when no command is entered in the command line
+        '''
+        return
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
