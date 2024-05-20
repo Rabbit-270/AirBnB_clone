c74b8bc70234f7c542a5288f252e272a57be6ed1
APINDA <apinda127@gmail.com>
2024-05-20 13:50:47 -0700

    unittesting file_storage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_models import BaseModel
 
 
 class TestFileEngine(unittest.TestCase):
@@ -8,12 +9,40 @@ class TestFileEngine(unittest.TestCase):
 Test cases for file_storage
     '''
     def test_all(self):
+        '''
+        Tests the returned JSON model
+        returned when the FileStorage is commanded
+        to return all of the objects.
+        '''
         localStorage = FileStorage()
         available_models = localStorage.save()
         '''
         the file should not be empty; False
         '''
         assertIsNone(available_models)
+        assertNotIsInstance(available_models, BaseModel)
+
+    def test_new(self):
+        '''
+        Tests the addition of a
+        new object in the File
+        '''
+        compactFile = FileStorage()
+        testObject = BaseModel()
+        compactFile.new(testObject)
+        '''
+        Make sure a BaseModel is being inserted
+        '''
+        assertIsNotNone(testObject.id)
+        '''
+        Make sure it is saved in the file/ __objects
+        '''
+        newFileStorage = compactFile.all()
+        key = "{}.{}".format(testObject.__class__.__name__, testObject.id)
+        assertIn(key, newFileStorage.keys())
+
+    def test_save(self):
+        pass
 
 
 if __name__ == '__main__':
