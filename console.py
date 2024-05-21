4ee97e2aec41444d595c00556bf28791eac4e8b8
Rabbit-270 <135014716+Rabbit-270@users.noreply.github.com>
2024-05-21 00:33:09 -0700

    update console.py(destroy)

--- console.py
+++ console.py
@@ -87,6 +87,48 @@ provided
                 print("** no instance found **")
             elif whitespaces == 0:
                 print("** instance id missing **")
+    def do_destroy(self, argv):
+        '''
+    Deletes an instance based on the id and class name given
+        '''
+        ObjectFound = False
+        if len(argv) == 0:
+            print("** class name missing **")
+        else:
+            whitespaces = 0
+            index = -1
+            counter = 0
+            while counter < len(argv):
+                currChar = argv[counter]
+                if currChar != " ":
+                    counter += 1
+                else:
+                    index = counter
+                    whitespaces += 1
+                    break
+            if whitespaces == 0:
+                print("** instance id missing **")
+            elif index != -1:
+                '''
+                both id and class name are provided
+                '''
+                classNameFromCommand = argv[:index]
+                idFromCommand = argv[(index + 1):]
+                if classNameFromCommand != 'BaseModel':
+                    print("** class doesn't exist **")
+                else:
+                    KeyGenerated = "{}.{}".format(classNameFromCommand, idFromCommand)
+                    OBJECTS = storage.all()
+                    for key in OBJECTS.keys():
+                        if key == KeyGenerated:
+                            ObjectFound = True
+                            del OBJECTS[KeyGenerated]
+                            for KEY in OBJECTS.keys():
+                                updateObjectVal = BaseModel(**OBJECTS[KEY])
+                                storage.new(updateObjectVal)
+                            storage.save()
+            elif ObjectFound is not True:
+                print("** no instance found **")
 
 
 if __name__ == "__main__":
