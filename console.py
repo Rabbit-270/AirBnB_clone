19caabeae0c4bbfaaf41f0e6ec868b785b0cb74c
APINDA <apinda127@gmail.com>
2024-05-20 17:00:33 -0700

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -11,24 +11,17 @@ class HBNBCommand(cmd.Cmd):
     Our command line
     '''
     def do_EOF(self, Line):
-        '''
-        Parse end-of-file-marker in the hbnb commnd line
-        and return True to terminate the program
-        '''
+        ''' Terminate the program using Ctrl+D'''
         return True
 
     def do_quit(self, line):
-        '''
-exit the program
-        '''
+        ''' Quit command to exit the program '''
         return True
 
     prompt = "(hbnb) "
 
     def emptyline(self):
-        '''
-Executed when no command is entered in the command line
-        '''
+        ''' Executed when no command is entered in the command line '''
         pass
 
 
