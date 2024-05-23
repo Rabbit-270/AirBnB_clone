bb20c5e50ce10637699bede2255fe10bdfa530ae
Rabbit-270 <135014716+Rabbit-270@users.noreply.github.com>
2024-05-23 12:44:35 -0700

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,2 +1,43 @@
 #!/usr/bin/python3
-""""""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
+
+class TestUser(unittest.TestCase):
+  '''
+  unittests to make sure that the
+  User class operates accordingly.
+  '''
+  FirstUser = User()
+  FirstUser.tested = False
+  def test_constructor(self):
+    '''
+    Unittests for testing the Constructor
+    of a User object
+    '''
+    self.assertIs(FirstUser, BaseModel)
+    self.assertIs(FirstUser, User)
+
+  def test_to_dict(self):
+    '''
+    Unittests for testing if the to_dict()
+    class method works for User object.
+    '''
+    returnedDict = FirstUser.to_dict()
+    if self.assertTrue(hasattr(FirstUser,"tested")) == True:
+      FirstUser.tested = False
+
+  def test_save(self):
+    '''
+    Tests if the save() class method
+    if it works.
+    '''
+    prev_updated_at = FirstUser.updated_at
+    FirstUser.save()
+    new_updated_at = FirstUser.updated_at
+    self.assertNotEqual(prev_updated_at, new_updated_at)
+
+
+if __name__=='__main__':
+  unittest.main()
