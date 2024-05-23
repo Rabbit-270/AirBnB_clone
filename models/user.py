7c2b254abe28608f4cec9bab5193673b6a8b724e
APINDA <apinda127@gmail.com>
2024-05-22 18:03:18 -0700

    updated user.py

--- models/user.py
+++ models/user.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-class User(BaseModel):
-  '''
-  Inherits from BaseModel
-  '''
-  def __init__(self):
-    ''' constructor '''
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
 
+class User(BaseModel):
+    '''
+    Inherits from BaseModel
+    '''
+    def __init__(self):
+        ''' constructor '''
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
