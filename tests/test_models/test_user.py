d127d3cac45bd2e9d5d535849fcbde035eb0c372
APINDA <apinda127@gmail.com>
2024-05-23 13:13:30 -0700

    unittests for user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,42 +6,40 @@ from models import storage
 
 
 class TestUser(unittest.TestCase):
-	'''
-  	unittests to make sure that the
- 	User class operates accordingly.
-  	'''
-  	def test_constructor(self):
-    	'''
-    	Unittests for testing the Constructor
-    	of a User object
-    	'''
-	    FirstUser = User()
-	    self.assertIs(FirstUser, BaseModel)
-	    self.assertIs(FirstUser, User)
+    '''
+    unittests to make sure that the
+    User class operates accordingly.
+    '''
+    def test_contructor(self):
+        '''
+        Unittests to make sure that the
+        User class object is created.
+        '''
+        FirstUser = User()
+        self.assertIs(FirstUser, BaseModel)
+        self.assertIs(FirstUser, User)
 
-	  def test_to_dict(self):
-	    '''
-	    Unittests for testing if the to_dict()
-	    class method works for User object.
-	    '''
-	    FirstUser = User()
-	    FirstUser.tested = False
-	    returnedDict = FirstUser.to_dict()
-	    if self.assertTrue(hasattr(FirstUser,"tested")) == True:
-	      FirstUser.tested = False
+    def test_to_dict(self):
+        '''
+        Unittests for testing if the to_dict()
+        class method works for User object.
+        '''
+        SecondUser = User()
+        SecondUser.tested = False
+        returnedDict = SecondUser.to_dict()
+        if self.assertTrue(hasattr(returnedDict, "tested")) is True:
+            SecondUser.tested = True
 
-	  def test_save(self):
-	    '''
-	    Tests if the save() class method
-	    if it works.
-	    '''
-	    FirstUser = User()
-	    FirstUser.tested = False
-	    prev_updated_at = FirstUser.updated_at
-	    FirstUser.save()
-	    new_updated_at = FirstUser.updated_at
-	    self.assertNotEqual(prev_updated_at, new_updated_at)
+    def test_save(self):
+        '''
+        Tests if the save() class method works.
+        '''
+        ThirdUser = User()
+        prev_updated_at = ThirdUser.updated_at
+        ThirdUser.save()
+        new_updated_at = ThirdUser.updated_at
+        self.assertIsNotEqual(prev_updated_at, new_updated_at)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
