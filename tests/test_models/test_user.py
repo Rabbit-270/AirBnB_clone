405523f42534ebd5dd6d809ea0d1c659066b28b5
Rabbit-270 <135014716+Rabbit-270@users.noreply.github.com>
2024-05-23 12:53:26 -0700

    Update test_user.py. Fixed indents

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,42 +4,43 @@ from models.base_model import BaseModel
 from models.user import User
 from models import storage
 
+
 class TestUser(unittest.TestCase):
-  '''
-  unittests to make sure that the
-  User class operates accordingly.
-  '''
-  def test_constructor(self):
-    '''
-    Unittests for testing the Constructor
-    of a User object
-    '''
-    FirstUser = User()
-    self.assertIs(FirstUser, BaseModel)
-    self.assertIs(FirstUser, User)
+	'''
+  	unittests to make sure that the
+ 	User class operates accordingly.
+  	'''
+  	def test_constructor(self):
+    	'''
+    	Unittests for testing the Constructor
+    	of a User object
+    	'''
+	    FirstUser = User()
+	    self.assertIs(FirstUser, BaseModel)
+	    self.assertIs(FirstUser, User)
 
-  def test_to_dict(self):
-    '''
-    Unittests for testing if the to_dict()
-    class method works for User object.
-    '''
-    FirstUser = User()
-    FirstUser.tested = False
-    returnedDict = FirstUser.to_dict()
-    if self.assertTrue(hasattr(FirstUser,"tested")) == True:
-      FirstUser.tested = False
+	  def test_to_dict(self):
+	    '''
+	    Unittests for testing if the to_dict()
+	    class method works for User object.
+	    '''
+	    FirstUser = User()
+	    FirstUser.tested = False
+	    returnedDict = FirstUser.to_dict()
+	    if self.assertTrue(hasattr(FirstUser,"tested")) == True:
+	      FirstUser.tested = False
 
-  def test_save(self):
-    '''
-    Tests if the save() class method
-    if it works.
-    '''
-    FirstUser = User()
-    FirstUser.tested = False
-    prev_updated_at = FirstUser.updated_at
-    FirstUser.save()
-    new_updated_at = FirstUser.updated_at
-    self.assertNotEqual(prev_updated_at, new_updated_at)
+	  def test_save(self):
+	    '''
+	    Tests if the save() class method
+	    if it works.
+	    '''
+	    FirstUser = User()
+	    FirstUser.tested = False
+	    prev_updated_at = FirstUser.updated_at
+	    FirstUser.save()
+	    new_updated_at = FirstUser.updated_at
+	    self.assertNotEqual(prev_updated_at, new_updated_at)
 
 
 if __name__=='__main__':
