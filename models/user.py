ce7dacd3e62142f8166274aa85c22b30515634f5
Rabbit-270 <135014716+Rabbit-270@users.noreply.github.com>
2024-05-22 17:53:43 -0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-""""""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+  '''
+  Inherits from BaseModel
+  '''
+  def __init__(self):
+    ''' constructor '''
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
